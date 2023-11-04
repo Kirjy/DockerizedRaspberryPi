0f8dc7523859d8365a5daf4d47f621ef9264c114
Kirjava <pymiguet@gmail.com>
2023-11-04 16:49:07 +0100

    Update UpdateConbee2.sh

--- z2m/UpdateConbee2.sh
+++ z2m/UpdateConbee2.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --entrypoint "/firmware-update.sh" --privileged --cap-add=ALL -v /dev:/dev -v /lib/modules:/lib/modules -v /sys:/sys deconzcommunity/deconz
+docker run -it --rm --entrypoint "/firmware-update.sh" --privileged --cap-add=ALL -v /dev:/dev -v /lib/modules:/lib/modules -v /sys:/sy -v ./temp:/usr/share/deCONZ/firmware/ deconzcommunity/deconz
